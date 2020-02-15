208aa7a411b9d9c267b09ed2208cabf8863c69fa
Alexander Ruppert <aruppert@pm.me>
2020-02-15 20:05:52 +0100

    Add modern setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="./logo192.png" alt="Sneaker Planet Shop Logo" />
+        <p>Sneaker Planet Shop Prototype with Bootstrap</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
