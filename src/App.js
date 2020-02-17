9b2b1dcaf751ceb4d68425be8b671ecbd2af1b1d
Alexander Ruppert <aruppert@pm.me>
2020-02-17 18:41:16 +0100

    Add react-router, theming and GlobalStyles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from "react";
+import GlobalStyles from "./GlobalStyles";
+import { ThemeProvider } from "emotion-theming";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { primary } from "./themes/themes";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="./logo192.png" alt="Sneaker Planet Shop Logo" />
-        <p>Sneaker Planet Shop Prototype with Bootstrap</p>
-      </header>
-    </div>
+    <>
+      <ThemeProvider theme={primary}>
+        <Router>
+          <GlobalStyles />
+          <Switch>
+            <Route path="/home">
+              <img src="./logo192.png" alt="Sneaker Planet Shop Logo" />
+              <p>Sneaker Planet Shop Prototype with Bootstrap</p>
+            </Route>
+          </Switch>
+        </Router>
+      </ThemeProvider>
+    </>
   );
 }
 
