1916a23e5ccb608f7a658d1804503d1fd2786d85
Alexander Ruppert <aruppert@pm.me>
2020-02-21 14:11:24 +0100

    Add sneakers with info and map() them as product cards (#7)
    * Add 12 sneakers and map product cards
    
    * Fix deepscan issue

---
+++ src/misc/Shoes.js
@@ -0,0 +1,92 @@
+const shoes = [
+  {
+    id: 0,
+    name: "Converse Classic",
+    for: "both",
+    description: "The all-time classic in original design.",
+    price: "80",
+    img: "pictures/sneaker1.webp"
+  },
+  {
+    id: 1,
+    name: "McSport 1st Edition",
+    for: "male",
+    description: "Loved by many - perfect for Tennis.",
+    price: "93",
+    img: "pictures/sneaker2.webp"
+  },
+  {
+    id: 2,
+    name: "Squash Supreme",
+    for: "female",
+    description: "Win every match with this sneaker.",
+    price: "75",
+    img: "pictures/sneaker3.webp"
+  },
+  {
+    id: 3,
+    name: "Sky Blue Boots",
+    for: "female",
+    description: "Make even the clear blue sky jealous.",
+    price: "99",
+    img: "pictures/sneaker4.webp"
+  },
+  {
+    id: 4,
+    name: "Jordans BW Homme",
+    for: "male",
+    description: "Jump higher and land softer with Jordans.",
+    price: "96",
+    img: "pictures/sneaker5.webp"
+  },
+  {
+    id: 5,
+    name: "Nike High Skate",
+    for: "both",
+    description: "Not only for skating the best shoe around.",
+    price: "88",
+    img: "pictures/sneaker6.webp"
+  },
+  {
+    id: 6,
+    name: "Nike Eco Rain",
+    for: "both",
+    description: "100% waterproof for every climate zone.",
+    price: "120",
+    img: "pictures/sneaker7.webp"
+  },
+  {
+    id: 7,
+    name: "Ferrari Italia Sport",
+    for: "male",
+    description: "Be the fastest in your sport with Ferrari.",
+    price: "125",
+    img: "pictures/sneaker8.webp"
+  },
+  {
+    id: 8,
+    name: "Purple Marathon .2",
+    for: "both",
+    description: "Fly through any race with our new improved model.",
+    price: "130",
+    img: "pictures/sneaker9.webp"
+  },
+  {
+    id: 9,
+    name: "adidas Jumping Elle",
+    for: "female",
+    description: "Great light weight sneaker with long lasting build.",
+    price: "110",
+    img: "pictures/sneaker11.webp"
+  },
+  {
+    id: 10,
+    name: "L.Vuitton meets Nike",
+    for: "male",
+    description: "Be aware winners. This shoe shows your success.",
+    price: "150",
+    img: "pictures/sneaker12.webp"
+  }
+];
+
+export default shoes;
